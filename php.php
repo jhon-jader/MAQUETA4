6b66e77b0dcee30e65123fb2740363b1aeb0dd62
jhon-jader <jhonjaderrojas27@gmail.com>
2021-08-05 01:13:10 +0200

    primera version

---
+++ php.php
@@ -0,0 +1,13 @@
+<?php
+
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$correo = $_POST['correo'];
+
+echo "BIENVENIDO <br>";
+echo "Nombre: ". $nombre . "<br>";
+echo "Apellido: ". $apellido. "<br>";
+echo "Correo: " . $correo . "<br>";
+
+
+?>
